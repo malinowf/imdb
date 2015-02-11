8dc28d577ec5ff9cde4e48146fec6bfb9fa9807a
Raghu Betina <rbetina@github.com>
2015-02-11 14:09:15 -0600

    Adds association methods between Movies and Roles

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -5,4 +5,8 @@ class Movie < ActiveRecord::Base
     return Director.find_by({ :id => self.director_id })
   end
 
+  def roles
+    return Role.where({ :movie_id => self.id })
+  end
+
 end
--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,7 @@
 class Role < ActiveRecord::Base
+
+  def movie
+    return Movie.find_by({ :id => self.movie_id })
+  end
+
 end
