a0a075ab5777e1c30583a4e4a814a1b4978479d5
Raghu Betina <rbetina@github.com>
2015-02-13 16:08:43 -0600

    Adds many-to-many methods

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -2,14 +2,15 @@ class Actor < ActiveRecord::Base
 
   validates :name, :presence => true, :uniqueness => true
 
-  # has many Roles (call the method "roles")
-
   has_many :roles, :class_name => "Role", :foreign_key => "actor_id"
 
-  # def roles
-  #   return Role.where :actor_id => self.id
-  # end
+  has_many :movies, :through => :roles
 
+  # def filmography
+  #   array_of_my_movie_ids = self.roles.pluck(:movie_id)
+
+  #   return Movie.where :id => array_of_my_movie_ids
+  # end
 
 end
 
